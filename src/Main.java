6741b78dd27ebb23d3820ff87896954375a1ecb7
Akip2 <126561427+Akip2@users.noreply.github.com>
2024-06-18 23:29:30 +0200

    Ajout du flou Gausien

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import flou.FlouMoyenne;
+import flou.Gausien;
 
 public class Main {
 
     public static void main(String[] args) {
-        FlouMoyenne flouMoyenne = new FlouMoyenne("img/planete1.jpg", "img/flouMoyenne.jpg");
-        flouMoyenne.flouter(21);
+        //FlouMoyenne flouMoyenne = new FlouMoyenne("img/planete1.jpg", "img/flouMoyenne.jpg");
+        //flouMoyenne.flouter(21);
+
+        Gausien gausien = new Gausien("img/planete1.jpg", "img/flouGausien2.jpg");
+        gausien.flouter(27, 20);
     }
 }
--- src/flou/Gausien.java
+++ src/flou/Gausien.java
@@ -1,63 +1,89 @@
 package flou;
-import outil.OutilCouleur;
-
-import java.io.File;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class Gausien{
-    public static void flouter(String fileName, String fileOutputName, double ecartType){
-        File file=new File(fileName);
+public class Gausien {
+    private String fileName;
+    private String fileOutput;
 
-        BufferedImage bufferedImage=null;
+    public Gausien(String fileName, String fileOutput) {
+        this.fileName = fileName;
+        this.fileOutput = fileOutput;
+    }
 
-        try {
-            bufferedImage=ImageIO.read(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void flouter(int coeff, double ecartType) {
+        if (coeff > 1 && coeff % 2 != 0) {
+            try {
+                File file = new File(fileName);
+                BufferedImage image = ImageIO.read(file);
+                BufferedImage imageCopy = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
 
+                double[][] kernel = genererKernel(coeff, ecartType);
 
-        int lengthX=bufferedImage.getWidth();
-        int lengthY=bufferedImage.getHeight();
-
-        BufferedImage bufferedOutput= new BufferedImage(lengthX, lengthY, BufferedImage.TYPE_3BYTE_BGR);
-
-        int[] pixelCentral={lengthX/2, lengthY/2};
-
-        for(int y=0; y<lengthY; y++){
-            for(int x=0; x<lengthX; x++){
-                double distX=Math.sqrt(Math.pow(x-pixelCentral[0], 2));
-                double distY=Math.sqrt(Math.pow(y-pixelCentral[1], 2));
-
-                double coef=genererCoef(distX, distY, ecartType);
-
-                int newRGB=(int)(bufferedImage.getRGB(x,y)*coef);
-
-                bufferedOutput.setRGB(x,y,newRGB);
+                for (int x = 0; x < image.getWidth(); x++) {
+                    for (int y = 0; y < image.getHeight(); y++) {
+                        int rgb = appliquerKernel(image, x, y, kernel);
+                        imageCopy.setRGB(x, y, rgb);
+                    }
+                }
+                ImageIO.write(imageCopy, "jpg", new File(fileOutput));
+            } catch (IOException e) {
+                System.out.println("Erreur lors de la copie de l'image");
             }
-        }
-
-        try {
-            ImageIO.write(bufferedOutput, "PNG", new File(fileOutputName));
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("Erreur de coefficient, il doit être impair.");
         }
     }
 
-    private static double genererCoef(double x, double y, double ecartType){
-        double res=Math.pow(
-                (1/(2*Math.PI*Math.pow(ecartType, 2))),
-                Math.exp(
-                        -(
-                                (Math.pow(x,2)+ Math.pow(y, 2))/
-                                        (2*Math.pow(ecartType,2))
-                        )
-                )
-        );
+    //Genere la matrice de coeffs servant de filtre
+    private double[][] genererKernel(int taille, double ecartType) {
+        double[][] kernel = new double[taille][taille];
+        int millieu = taille / 2;
+        double somme = 0.0;
 
-        return res;
+        for (int x = -millieu; x <= millieu; x++) {
+            for (int y = -millieu; y <= millieu; y++) {
+                double exponent = -(x * x + y * y) / (2 * ecartType * ecartType);
+                kernel[x + millieu][y + millieu] = Math.exp(exponent) / (2 * Math.PI * ecartType * ecartType);
+                somme += kernel[x + millieu][y + millieu];
+            }
+        }
+
+        // Normalisation des coefs
+        for (int i = 0; i < taille; i++) {
+            for (int j = 0; j < taille; j++) {
+                kernel[i][j] /= somme;
+            }
+        }
+        return kernel;
+    }
+    //application du filtre
+    private int appliquerKernel(BufferedImage image, int x, int y, double[][] kernel) {
+        int milieu = kernel.length / 2;
+        double red = 0, green = 0, blue = 0;
+        for (int i = -milieu; i <= milieu; i++) {
+            for (int j = -milieu; j <= milieu; j++) {
+                int imgX = x + i;
+                int imgY = y + j;
+                if (imgX < 0 || imgX >= image.getWidth() || imgY < 0 || imgY >= image.getHeight()) {
+                    continue;
+                }
+
+                Color color = new Color(image.getRGB(imgX, imgY));
+                double coeff = kernel[i + milieu][j + milieu];
+                red += color.getRed() * coeff;
+                green += color.getGreen() * coeff;
+                blue += color.getBlue() * coeff;
+            }
+        }
+        int r = Math.min(255, Math.max(0, (int) red));
+        int g = Math.min(255, Math.max(0, (int) green));
+        int b = Math.min(255, Math.max(0, (int) blue));
+
+        return new Color(r, g, b).getRGB();
     }
 }
