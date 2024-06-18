24ce097e086282297b14d6a70ef6190113ec093d
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2024-06-18 15:14:13 +0200

    flouMoyenne

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import flou.FlouMoyenne;
 public class Main {
 
     public static void main(String[] args) {
-        FlouMoyenne flouMoyenne = new FlouMoyenne("img/planete1.jpg");
-        flouMoyenne.flouter(3, 3);
+        FlouMoyenne flouMoyenne = new FlouMoyenne("img/planete1.jpg", "img/flouMoyenne.jpg");
+        flouMoyenne.flouter(6);
     }
 }
--- src/flou/FlouMoyenne.java
+++ src/flou/FlouMoyenne.java
@@ -1,39 +1,67 @@
 package flou;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
-import outil.OutilCouleur;
+import java.util.ArrayList;
 
 public class FlouMoyenne {
 
     private String fileName;
+    private String fileOutput;
 
-    public FlouMoyenne(String fileName) {
+    public FlouMoyenne(String fileName, String fileOutput) {
         this.fileName = fileName;
+        this.fileOutput = fileOutput;
     }
 
-    public void flouter(int largeur, int hauteur) {
+    public void flouter(int coeff) {
         try {
             File file = new File(fileName);
             BufferedImage image = ImageIO.read(file);
             BufferedImage imageCopy = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
             for (int x = 0; x < image.getWidth(); x++) {
                 for (int y = 0; y < image.getHeight(); y++) {
-                    int[] tabColor = OutilCouleur.getTabColor(image.getRGB(x, y));
-                    int red = tabColor[0];
-                    int green = tabColor[1];
-                    int blue = tabColor[2];
-                    int redMoyenne = 0;
-                    int greenMoyenne = 0;
-                    int blueMoyenne = 0;
+                    ArrayList<Integer> voisins = getRGBVoisins(image, x, y, coeff);
+                    int moyenne = moyenne(voisins);
+                    imageCopy.setRGB(x, y, moyenne);
                 }
             }
-            ImageIO.write(imageCopy, "jpg", new File("flouMoyenne.jpg"));
+            ImageIO.write(imageCopy, "jpg", new File(fileOutput));
         } catch (IOException e) {
             System.out.println("Erreur lors de la copie de l'image");
         }
+    }
+
+    private ArrayList<Integer> getRGBVoisins(BufferedImage image, int x, int y, int coeff) {
+        ArrayList<Integer> rgbVoisins = new ArrayList<>();
+        for (int i = x - coeff; i <= x + coeff; i++) {
+            for (int j = y - coeff; j <= y + coeff; j++) {
+                try {
+                    rgbVoisins.add(image.getRGB(i, j));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    rgbVoisins.add(0);
+                }
+            }
+        }
+        return rgbVoisins;
+    }
+
+    private int moyenne(ArrayList<Integer> rgbVoisins) {
+        int sommeR = 0;
+        int sommeG = 0;
+        int sommeB = 0;
+        for (int rgb : rgbVoisins) {
+            Color color = new Color(rgb);
+            sommeR += color.getRed();
+            sommeG += color.getGreen();
+            sommeB += color.getBlue();
+        }
+        int moyenneR = sommeR / rgbVoisins.size();
+        int moyenneG = sommeG / rgbVoisins.size();
+        int moyenneB = sommeB / rgbVoisins.size();
+        return new Color(moyenneR, moyenneG, moyenneB).getRGB();
     }
 }
