47ef5c9a23ecf9a94e516a17b8f623b86ddb57b7
Ratz123323 <nathan.pierrot7@etu.univ-lorraine.fr>
2024-06-18 15:24:23 +0200

    fix flou

--- src/flou/FlouMoyenne.java
+++ src/flou/FlouMoyenne.java
@@ -18,27 +18,32 @@ public class FlouMoyenne {
     }
 
     public void flouter(int coeff) {
-        try {
-            File file = new File(fileName);
-            BufferedImage image = ImageIO.read(file);
-            BufferedImage imageCopy = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
-            for (int x = 0; x < image.getWidth(); x++) {
-                for (int y = 0; y < image.getHeight(); y++) {
-                    ArrayList<Integer> voisins = getRGBVoisins(image, x, y, coeff);
-                    int moyenne = moyenne(voisins);
-                    imageCopy.setRGB(x, y, moyenne);
+        if(coeff > 1 && coeff%2 != 0) {
+            try {
+                File file = new File(fileName);
+                BufferedImage image = ImageIO.read(file);
+                BufferedImage imageCopy = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
+                for (int x = 0; x < image.getWidth(); x++) {
+                    for (int y = 0; y < image.getHeight(); y++) {
+                        ArrayList<Integer> voisins = getRGBVoisins(image, x, y, coeff);
+                        int moyenne = moyenne(voisins);
+                        imageCopy.setRGB(x, y, moyenne);
+                    }
                 }
+                ImageIO.write(imageCopy, "jpg", new File(fileOutput));
+            } catch (IOException e) {
+                System.out.println("Erreur lors de la copie de l'image");
             }
-            ImageIO.write(imageCopy, "jpg", new File(fileOutput));
-        } catch (IOException e) {
-            System.out.println("Erreur lors de la copie de l'image");
+        }else{
+            System.out.println("Erreur de coefficient, il doit être impaire.");
         }
+
     }
 
     private ArrayList<Integer> getRGBVoisins(BufferedImage image, int x, int y, int coeff) {
         ArrayList<Integer> rgbVoisins = new ArrayList<>();
-        for (int i = x - coeff; i <= x + coeff; i++) {
-            for (int j = y - coeff; j <= y + coeff; j++) {
+        for (int i = x - ((coeff-1)/2); i <= x + ((coeff-1)/2); i++) {
+            for (int j = y - ((coeff-1)/2); j <= y + ((coeff-1)/2); j++) {
                 try {
                     rgbVoisins.add(image.getRGB(i, j));
                 } catch (ArrayIndexOutOfBoundsException e) {
